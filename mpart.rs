9e29b2a0046e7862336bbae02ef33b476682fc89
Sunil <sunil@live.com>
2026-02-07 18:13:27 -0800

    applying rustfmt

--- mpart.rs
+++ mpart.rs
@@ -1,17 +1,17 @@
 /// Represents the implementation of `multipart/form-data` formatted data.
 ///
 /// This will parse the source stream into an iterator over fields
-/// 
+///
 ///
 /// # Field Exclusivity
 /// source: https://andreubotella.github.io/multipart-form-data/
 /// and https://www.rfc-editor.org/rfc/rfc7578
-use std::{io::{Read,}};
+use std::io::Read;
 
 pub struct MPart<'a> {
     reader: &'a mut dyn Read,
     boundary: Vec<u8>,
-    buffer: [u8;4096],
+    buffer: [u8; 4096],
     bytes_read: usize,
     slice_start: usize,
     slice_end: usize,
@@ -20,14 +20,14 @@ pub struct MPart<'a> {
 }
 
 pub struct Part {
-    pub content_type : Option<String>,
-    pub content_name : String,
+    pub content_type: Option<String>,
+    pub content_name: String,
     pub total_read_ammount: usize,
     pub content_filename: Option<String>,
     pub content: Vec<u8>,
 }
 
-impl<'a> MPart <'a> {
+impl<'a> MPart<'a> {
     pub fn from(r: &'a mut impl Read, b: &[u8]) -> Self {
         MPart {
             reader: r,
@@ -40,20 +40,19 @@ impl<'a> MPart <'a> {
             last: false,
         }
     }
-    
+
     pub fn consumed(&self) -> usize {
         self.bytes_read
     }
-    
-    
+
     fn next_byte(&mut self) -> Option<u8> {
-        self.slice_start +=1;
+        self.slice_start += 1;
         if self.slice_start >= self.slice_end {
             let Ok(len) = self.reader.read(&mut self.buffer) else {
-                return None //Err(io::Error::new(ErrorKind::UnexpectedEof, "Failure - eof"))
+                return None; //Err(io::Error::new(ErrorKind::UnexpectedEof, "Failure - eof"))
             };
             if len == 0 {
-                return None // Err(io::Error::new(ErrorKind::UnexpectedEof, "Failure - eof"))
+                return None; // Err(io::Error::new(ErrorKind::UnexpectedEof, "Failure - eof"))
             }
             self.slice_start = 0;
             self.slice_end = len;
@@ -61,94 +60,92 @@ impl<'a> MPart <'a> {
         }
         Some(self.buffer[self.slice_start])
     }
-    
-    fn parse_name_line(&mut self) -> Option<(String,Option<String>)> {
+
+    fn parse_name_line(&mut self) -> Option<(String, Option<String>)> {
         let mut temp_stor = Vec::new();
         loop {
             let b = self.next_byte()?;
-            
+
             if b == 0x0d {
-                 let b2 = self.next_byte()?; 
-                 if b2 == 0x0a {
+                let b2 = self.next_byte()?;
+                if b2 == 0x0a {
                     if temp_stor.is_empty() {
-                        return None
+                        return None;
                     } else {
-                        let line = String::from_utf8(temp_stor).ok()?; 
+                        let line = String::from_utf8(temp_stor).ok()?;
                         //eprintln!{"dispt {line}"}
                         let line = adjust_to_pattern(line, "Ullllll-Ullllllllll: llll-llll; llll");
                         match line.strip_prefix("Content-Disposition: form-data; name=\"") {
                             Some(line) => {
-                                let Some((name,file)) = line.split_once('"') else {
-                                    return Some((String::from(""), None))
+                                let Some((name, file)) = line.split_once('"') else {
+                                    return Some((String::from(""), None));
                                 };
                                 if !file.is_empty() {
                                     let file = adjust_to_pattern(file.to_string(), "; llllllll");
                                     if let Some(file) = file.strip_prefix("; filename=\"") {
-                                        let Some((file,_)) = file.split_once('"') else {
-                                            return Some((String::from(name), None))
+                                        let Some((file, _)) = file.split_once('"') else {
+                                            return Some((String::from(name), None));
                                         };
-                                        return Some((String::from(name), Some(String::from(file))))
+                                        return Some((String::from(name), Some(String::from(file))));
                                     }
                                 }
-                                return Some((String::from(name), None))
+                                return Some((String::from(name), None));
                             }
-                            _ => return None //Err(io::Error::new(ErrorKind::Other, "Failure - no content disposition"))
+                            _ => return None, //Err(io::Error::new(ErrorKind::Other, "Failure - no content disposition"))
                         }
                     }
-                 } else {
-                    if b2 ==  0x0d {
+                } else {
+                    if b2 == 0x0d {
                         // should be failure
-                         return None //Ok((String::from(""), None))
+                        return None; //Ok((String::from(""), None))
                     }
-                     temp_stor.push(b2)
-                 }
-                 
+                    temp_stor.push(b2)
+                }
             } else {
                 temp_stor.push(b)
             }
         }
     }
-    
+
     fn parse_type_line(&mut self) -> Option<String> {
         let mut temp_stor = Vec::new();
         loop {
             let b = self.next_byte()?;
-            
+
             if b == 0x0d {
-                 let b2 = self.next_byte()?; 
-                 if b2 == 0x0a {
+                let b2 = self.next_byte()?;
+                if b2 == 0x0a {
                     if temp_stor.is_empty() {
-                        return None
+                        return None;
                     } else {
                         return match String::from_utf8(temp_stor) {
                             Ok(res) => {
                                 let res = adjust_to_pattern(res, "Ullllll-Ulll:");
-                                match res.strip_prefix("Content-Type: ") { 
-                                   Some(res) => Some(res.to_string()), // can be parsed further for ;charset=UTF-8
-                                    _ => Some(String::new())
+                                match res.strip_prefix("Content-Type: ") {
+                                    Some(res) => Some(res.to_string()), // can be parsed further for ;charset=UTF-8
+                                    _ => Some(String::new()),
                                 }
                             }
-                            _ => Some("".to_string())
-                        }
+                            _ => Some("".to_string()),
+                        };
                     }
-                 } else {
-                     temp_stor.push(b2)
-                 }
+                } else {
+                    temp_stor.push(b2)
+                }
             } else {
                 temp_stor.push(b)
             }
         }
     }
-    
 }
 
 impl Iterator for MPart<'_> {
     type Item = Part;
-    
+
     //let temp = std::env::var(String::from("TEMP")).unwrap();
     fn next(&mut self) -> Option<Self::Item> {
         if self.last {
-            return None
+            return None;
         }
         if self.first {
             let b = self.next_byte()?;
@@ -156,11 +153,11 @@ impl Iterator for MPart<'_> {
             if b == 0x2D && b2 == 0x2D {
                 for i in 0..self.boundary.len() {
                     if self.next_byte()? != self.boundary[i] {
-                        return None
+                        return None;
                     }
                 }
             } else {
-                return None
+                return None;
             }
             let b = self.next_byte()?;
             let b2 = self.next_byte()?;
@@ -170,29 +167,28 @@ impl Iterator for MPart<'_> {
                     let b = self.next_byte()?;
                     let b2 = self.next_byte()?;
                     if b == 0x0d && b2 == 0x0a {
-                        return None
+                        return None;
                     }
                 }
             }
-            
+
             self.first = false
         }
         // read and parse line after boundary
-        let (name,filename) = self.parse_name_line()?;
-        let content_type = 
-            match self.parse_type_line() {
-                None => Some("text/plain".to_string()),
-                Some(text) => {
-                    // read empty line
-                    let b = self.next_byte()?;
-                    let b2 = self.next_byte()?;
-                    if b != 0x0d || b2 != 0x0a {
-                        // TODO can be one more line as
-                        // content-transfer-encoding: quoted-printable
-                        return None
-                    }
-                    Some(text.to_string())
+        let (name, filename) = self.parse_name_line()?;
+        let content_type = match self.parse_type_line() {
+            None => Some("text/plain".to_string()),
+            Some(text) => {
+                // read empty line
+                let b = self.next_byte()?;
+                let b2 = self.next_byte()?;
+                if b != 0x0d || b2 != 0x0a {
+                    // TODO can be one more line as
+                    // content-transfer-encoding: quoted-printable
+                    return None;
                 }
+                Some(text.to_string())
+            }
         };
         //eprintln!{"read content of {name} and type {content_type:?}"}
         let mut content = Vec::new();
@@ -207,17 +203,17 @@ impl Iterator for MPart<'_> {
                     temp_stor.push(b);
                     for i in 0..self.boundary.len() {
                         let bn = self.next_byte()?;
-                        if  bn != self.boundary[i] {
+                        if bn != self.boundary[i] {
                             content.append(&mut temp_stor);
                             content.push(bn);
-                            break
+                            break;
                         }
                         temp_stor.push(bn)
                     }
-                    if temp_stor.len() == self.boundary.len()+2 {
+                    if temp_stor.len() == self.boundary.len() + 2 {
                         let b = self.next_byte()?;
                         let b2 = self.next_byte()?;
-                        
+
                         if b == 0x0d && b2 == 0x0a || b == 0x2D && b2 == 0x2D {
                             if b == 0x2D && b2 == 0x2D {
                                 let b = self.next_byte()?;
@@ -225,22 +221,22 @@ impl Iterator for MPart<'_> {
                                 // check they are 0d 0a
                                 if b != 0x0d || b2 != 0x0a {
                                     //eprintln!{"no end line"}
-                                    return None
+                                    return None;
                                 }
                                 self.last = true
                             }
                             // remove \r\n
                             content.truncate(content.len() - 2);
                             return Some(Part {
-                                   content_type,
-                                    content_name : name,
-                                    total_read_ammount: self.bytes_read,
-                                    content_filename: filename,
-                                    content
-                                 })
+                                content_type,
+                                content_name: name,
+                                total_read_ammount: self.bytes_read,
+                                content_filename: filename,
+                                content,
+                            });
                         } else {
                             //eprintln!{"tail after sep bndry -- {b} {b2}"}
-                        }  
+                        }
                     } else {
                         //eprintln!{"boundary {} found {}",self.boundary.len(), temp_stor.len()}
                     }
@@ -257,10 +253,18 @@ impl Iterator for MPart<'_> {
 
 fn adjust_to_pattern(mut s: String, pat: &str) -> String {
     let content = unsafe { s.as_bytes_mut() };
-    for (i,c) in pat.chars().enumerate() {
+    for (i, c) in pat.chars().enumerate() {
         match c {
-            'U' => if content[i] >= 97 && content[i] <= 122 {content[i] -= 32},
-            'l' => if content[i] >= 65 && content[i] <= 90 {content[i] += 32},
+            'U' => {
+                if content[i] >= 97 && content[i] <= 122 {
+                    content[i] -= 32
+                }
+            }
+            'l' => {
+                if content[i] >= 65 && content[i] <= 90 {
+                    content[i] += 32
+                }
+            }
             _ => (),
         }
     }
--- test/test.rs
+++ test/test.rs
@@ -1,13 +1,13 @@
-extern crate simweb;
 extern crate simtime;
-use std::fs::read_to_string;
-use std::time::SystemTime;
+extern crate simweb;
+use ControlFlow::Continue;
 use std::collections::HashMap;
+use std::fs::read_to_string;
+use std::ops::ControlFlow;
 #[cfg(any(unix, target_os = "redox"))]
 use std::os::unix::fs::PermissionsExt;
 use std::path::Path;
-use std::ops::ControlFlow;
-use ControlFlow::Continue;
+use std::time::SystemTime;
 
 use simweb::{WebPage, base64_encode_with_padding, url_encode};
 
@@ -16,33 +16,52 @@ struct Page {
 }
 
 // URL http://somepc:3000/rustcgi/webcgi/
-fn main()  {
-   Page { path:
-        if let Ok(current_path) = std::env::var(String::from("PATH_TRANSLATED")) {
-            Some (current_path.to_owned())
-        } else { None }
-    }.show()
+fn main() {
+    Page {
+        path: if let Ok(current_path) = std::env::var(String::from("PATH_TRANSLATED")) {
+            Some(current_path.to_owned())
+        } else {
+            None
+        },
+    }
+    .show()
 }
 
 impl simweb::WebPage for Page {
     fn main_load(&self) -> Result<String, Box<dyn std::error::Error + 'static>> {
-        Ok(include_str!{"test.html"}.to_string())
+        Ok(include_str! {"test.html"}.to_string())
     }
-    
-    fn apply_specific(&self, page_map: &mut HashMap<&str, String>) -> Result<(), Box<dyn std::error::Error + 'static>> {
-        page_map.insert("timestamp",
-           simweb::http_format_time(SystemTime::now()));
-        page_map.insert("Epoch", format!("{}", simweb::parse_http_timestamp(page_map.get("timestamp").unwrap()).unwrap()));
-        page_map.insert("current_path",
-          match &self.path { Some(path) => path.to_owned(), _ => "".to_string()});
-        page_map.insert("base64", base64_encode_with_padding("h75s!!@$#-`".as_bytes()));
+
+    fn apply_specific(
+        &self,
+        page_map: &mut HashMap<&str, String>,
+    ) -> Result<(), Box<dyn std::error::Error + 'static>> {
+        page_map.insert("timestamp", simweb::http_format_time(SystemTime::now()));
+        page_map.insert(
+            "Epoch",
+            format!(
+                "{}",
+                simweb::parse_http_timestamp(page_map.get("timestamp").unwrap()).unwrap()
+            ),
+        );
+        page_map.insert(
+            "current_path",
+            match &self.path {
+                Some(path) => path.to_owned(),
+                _ => "".to_string(),
+            },
+        );
+        page_map.insert(
+            "base64",
+            base64_encode_with_padding("h75s!!@$#-`".as_bytes()),
+        );
         page_map.insert("url_encode", url_encode("@@__/"));
         page_map.insert("content",
             match &self.path {
                 Some(path) => {
                     let path = Path::new(&path);
                     if path.is_file() {
-                        format!{"<pre>{}</pre>", read_to_string(&path).map_err(|e| format!{"{e:?}"})?} 
+                        format!{"<pre>{}</pre>", read_to_string(path).map_err(|e| format!{"{e:?}"})?} 
                     } else if path.is_dir() {
                         match path.read_dir() {
                             Ok(dir) => {
@@ -56,7 +75,7 @@ impl simweb::WebPage for Page {
                                     if el.is_empty() {
                                         ControlFlow::<String, String>::Continue(res)
                                     } else {
-                                        ControlFlow::Continue(res + "/" + &simweb::url_encode(&el))
+                                        ControlFlow::Continue(res + "/" + &simweb::url_encode(el))
                                     });
                                 let encoded = match encoded {
                                     Continue(res) => res,
@@ -64,23 +83,21 @@ impl simweb::WebPage for Page {
                                 };
                                 dir_cont.push_str(&format!{"<!-- {web_path} -> {encoded} --><table>"});
                                 let local = simweb::WebData::new().param("localtime").is_some();
-                                for entry in dir {
-                                    if let Ok(entry) = entry {
-                                        let file_name = entry.file_name().to_str().unwrap().to_owned();
-                                        let slash = if entry.file_type().unwrap().is_dir() { "/" } else { "" };
-                                        dir_cont.push_str(&format!("<tr><td id=\"rr0\"><a  href=\"./{1}{slash}\">{0}</a></td>", simweb::html_encode(&file_name),
-                                              simweb::url_encode(&file_name)));
-                                        let metadata = entry.metadata().map_err(|e| format!{"{e:?}"})?;
-                                        #[cfg(target_os = "windows")]
-                                        let mode = if metadata.permissions().readonly() {0o444}else{0o777};
-                                        #[cfg(any(unix, target_os = "redox"))]
-                                        let mode =  metadata.permissions().mode();
-                                        dir_cont.push_str(&format!{r#" <td>{}</td><td style="text-align: end; padding-right: 1em;">{}</td>
-                                        <td style="text-align: center; padding-right: 1em;">{:0>16}</td><td>{:0>3o}</td></tr>"#, 
-                                        if metadata.is_dir() { simweb::html_encode(&"<DIR>")} else  {"file".to_string()},
-                                        metadata.len(), 
-                                        format_time(metadata.modified().map_err(|e| format!{"{e:?}"})?, local), mode})
-                                    }
+                                for entry in dir.flatten() {
+                                    let file_name = entry.file_name().to_str().unwrap().to_owned();
+                                    let slash = if entry.file_type().unwrap().is_dir() { "/" } else { "" };
+                                    dir_cont.push_str(&format!("<tr><td id=\"rr0\"><a  href=\"./{1}{slash}\">{0}</a></td>", simweb::html_encode(&file_name),
+                                          simweb::url_encode(&file_name)));
+                                    let metadata = entry.metadata().map_err(|e| format!{"{e:?}"})?;
+                                    #[cfg(target_os = "windows")]
+                                    let mode = if metadata.permissions().readonly() {0o444}else{0o777};
+                                    #[cfg(any(unix, target_os = "redox"))]
+                                    let mode =  metadata.permissions().mode();
+                                    dir_cont.push_str(&format!{r#" <td>{}</td><td style="text-align: end; padding-right: 1em;">{}</td>
+                                    <td style="text-align: center; padding-right: 1em;">{:0>16}</td><td>{:0>3o}</td></tr>"#, 
+                                    if metadata.is_dir() { simweb::html_encode(&"<DIR>")} else {"file".to_string()},
+                                    metadata.len(), 
+                                    format_time(metadata.modified().map_err(|e| format!{"{e:?}"})?, local), mode})
                                 }
                                 dir_cont.push_str("</table>");
                                 dir_cont
@@ -93,15 +110,18 @@ impl simweb::WebPage for Page {
                 },
                 _ => "Nothig is here".to_owned()
             });
-            Ok(())
-        }
+        Ok(())
+    }
 }
-
 
 fn format_time(time: SystemTime, local: bool) -> String {
     let dur = time.duration_since(SystemTime::UNIX_EPOCH).unwrap();
-    let offset = if local { simtime::get_local_timezone_offset() * 60}
-    else {0};
-    let (y, m, d, h, min, _s, _w) = simtime::get_datetime(1970, ((dur.as_secs() as i64) + offset as i64) as u64);
+    let offset = if local {
+        simtime::get_local_timezone_offset() * 60
+    } else {
+        0
+    };
+    let (y, m, d, h, min, _s, _w) =
+        simtime::get_datetime(1970, ((dur.as_secs() as i64) + offset as i64) as u64);
     format!("{m}-{d:0>2}-{y:0>2} {h:0>2}:{min:0>2}")
 }
